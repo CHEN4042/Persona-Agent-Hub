4098b90039762474863a70827fd0ec6ac59d5e30
hche0427 <hche0427@uni.sydney.edu.au>
2025-08-29 18:01:27 +1000

    update

---
+++ src/main/java/com/haochen/personaagenthub/PersonaAgentHubApplication.java
@@ -0,0 +1,13 @@
+package com.haochen.personaagenthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaAgentHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonaAgentHubApplication.class, args);
+    }
+
+}
